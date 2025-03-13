86797da59d67d35ba55379dc7ede690ab3b0b71c
RAFAEL OLIVEIRA <rafael.oliveira901@etec.sp.gov.br>
2025-03-13 15:54:52 -0300

    Creating CadastroAlunos Model

--- TCCControleDeAcesso/Models/CadastroAlunos.cs
+++ TCCControleDeAcesso/Models/CadastroAlunos.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
 
 namespace TCCControleDeAcesso.Models
 {
-    internal class CadastroAlunos
+    public class CadastroAlunos
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string rm {  get; set; }
+        public int idCurso {  get; set; }
+        public int idEscola {  get; set; }
+        public int  serie {  get; set; }
+        public UInt16 foto { get; set; }
+        public UInt16 digital{ get; set; }
+
+
+
+        public void Insert()
+        {
+            try
+            {
+                Banco.OpenConnection();
+
+            Banco.Command = new MySqlCommand("insert into Alunos");
+
+            }
+            catch (Exception e) {
+            
+                MessageBox.Show(e.Message, "Erro Ao Inserir Dados!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            
+            }
+        }
     }
 }
