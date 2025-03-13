c8265b8f1586edeafbfbb04f078ccaf95c25b431
RAFAEL OLIVEIRA <rafael.oliveira901@etec.sp.gov.br>
2025-03-13 11:48:47 -0300

    Adicionar arquivos de projeto.

---
+++ TCCControleDeAcesso/Controllers/Login.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCCControleDeAcesso.Controllers
+{
+    internal class Login
+    {
+    }
+}
