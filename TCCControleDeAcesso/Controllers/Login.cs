89f006d2bf9b0f0390c384fa99f05d4685b1adc2
RAFAEL OLIVEIRA <rafael.oliveira901@etec.sp.gov.br>
2025-03-14 15:16:46 -0300

    Creating Function login | finisihing Cadastro Alunos Models

--- TCCControleDeAcesso/Controllers/Login.cs
+++ TCCControleDeAcesso/Controllers/Login.cs
@@ -3,10 +3,90 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+using TCCControleDeAcesso.Models;
 
 namespace TCCControleDeAcesso.Controllers
 {
-    internal class Login
+    public class Login
     {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string senha { get; set; }
+
+        public int idEscola;
+
+        public void LoginPermissions()
+        {
+            try
+            {
+
+                Banco.OpenConnection();
+
+
+                Banco.Command = new MySqlCommand("SELECT id FROM cadastros WHERE nome=@nome AND senha=@senha", Banco.Connection);
+
+
+                Banco.Command.Parameters.AddWithValue("@nome", nome);
+                Banco.Command.Parameters.AddWithValue("@senha", senha);
+
+
+                idEscola = Convert.ToInt32(Banco.Command.ExecuteScalar());
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                Banco.CloseConnection();
+            }
+        }
+
+
+        public void SignIn()
+        {
+            try
+            {
+
+                Banco.OpenConnection();
+
+
+                Banco.Command = new MySqlCommand("SELECT COUNT(*) FROM cadastros WHERE nome=@nome AND senha=@senha", Banco.Connection);
+
+
+                Banco.Command.Parameters.AddWithValue("@nome", nome);
+                Banco.Command.Parameters.AddWithValue("@senha", senha);
+
+      
+                int count = Convert.ToInt32(Banco.Command.ExecuteScalar());
+
+                Banco.CloseConnection();
+
+                if (count > 0)
+                {
+                    MessageBox.Show("Login realizado com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    LoginPermissions();
+                    frmMainMenu check = new frmMainMenu();
+                    check.Show();
+
+                }
+                else
+                {
+                    MessageBox.Show("Nome ou senha incorretos.", "Erro de Login", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                Banco.CloseConnection();
+            }
+        }
     }
 }
+
--- TCCControleDeAcesso/Models/CadastroAlunos.cs
+++ TCCControleDeAcesso/Models/CadastroAlunos.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO.Ports;
 using System.Linq;
 using System.Text;
@@ -14,12 +15,12 @@ namespace TCCControleDeAcesso.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string rm {  get; set; }
-        public int idCurso {  get; set; }
-        public int idEscola {  get; set; }
-        public int  serie {  get; set; }
+        public string rm { get; set; }
+        public int idCurso { get; set; }
+        public int idEscola { get; set; }
+        public int serie { get; set; }
         public UInt16 foto { get; set; }
-        public UInt16 digital{ get; set; }
+        public UInt16 digital { get; set; }
 
 
 
@@ -29,13 +30,82 @@ namespace TCCControleDeAcesso.Models
             {
                 Banco.OpenConnection();
 
-            Banco.Command = new MySqlCommand("insert into Alunos");
+                Banco.Command = new MySqlCommand("insert into Alunos(id, nome, rm, serie, idEscola, idCurso, foto, digital) " +
+                    "values(@id, @nome, @rm, @serie, @idEscola, @idCurso, @foto, @digital)", Banco.Connection);
+                Banco.Command.Parameters.AddWithValue("@id", Id);
+                Banco.Command.Parameters.AddWithValue("@nome", Name);
+                Banco.Command.Parameters.AddWithValue("@rm", rm);
+                Banco.Command.Parameters.AddWithValue("@serie", serie);
+                Banco.Command.Parameters.AddWithValue("@idEscola", idEscola);
+                Banco.Command.Parameters.AddWithValue("@idCurso", idCurso);
+                Banco.Command.Parameters.AddWithValue("@foto", foto);
+                Banco.Command.Parameters.AddWithValue("@digital", digital);
+                Banco.Command.ExecuteNonQuery();
+
+                Banco.CloseConnection();
+
 
             }
-            catch (Exception e) {
-            
+            catch (Exception e)
+            {
+
                 MessageBox.Show(e.Message, "Erro Ao Inserir Dados!", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            
+
+            }
+        }
+
+        public void Delete()
+        {
+            try
+            {
+                Banco.OpenConnection();
+                Banco.Command = new MySqlCommand("delete from Alunos where id = @id", Banco.Connection);
+                Banco.Command.Parameters.AddWithValue("@id", Id);
+                Banco.Command.ExecuteNonQuery();
+                Banco.CloseConnection();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Erro Ao Deletar Dados!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public void Update()
+        {
+            try
+            {
+                Banco.OpenConnection();
+                Banco.Command = new MySqlCommand("update Alunos set nome = @nome, rm = @rm, serie = @serie, idEscola = @idEscola, idCurso = @idCurso, foto = @foto, digital = @digital where id = @id", Banco.Connection);
+                Banco.Command.Parameters.AddWithValue("@id", Id);
+                Banco.Command.Parameters.AddWithValue("@nome", Name);
+                Banco.Command.Parameters.AddWithValue("@rm", rm);
+                Banco.Command.Parameters.AddWithValue("@serie", serie);
+                Banco.Command.Parameters.AddWithValue("@idEscola", idEscola);
+                Banco.Command.Parameters.AddWithValue("@idCurso", idCurso);
+                Banco.Command.Parameters.AddWithValue("@foto", foto);
+                Banco.Command.Parameters.AddWithValue("@digital", digital);
+                Banco.Command.ExecuteNonQuery();
+                Banco.CloseConnection();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Erro Ao Atualizar Dados!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public void ListStudents()
+        {
+            try
+            {
+                Banco.OpenConnection();
+                Banco.DataAdapter = new MySqlDataAdapter("select a.id, a.nome, a.rm , a.serie, a.foto, a.digital from Alunos a inner join escolas on a.idEscola=id", Banco.Connection);
+                Banco.datTable = new DataTable();
+                Banco.DataAdapter.Fill(Banco.datTable);
+                Banco.CloseConnection();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Erro Ao Listar Dados!", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
     }
