5f475676feefcf5e8bf36e1ff1c3631447b0936d
BrunoFreitas09 <brunootaviocostadefreitas@gmail.com>
2025-05-29 15:35:38 -0300

    Termino da insercao de cadsatro no banco

--- TCCControleDeAcesso/Views/frmAtivacaoConta.cs
+++ TCCControleDeAcesso/Views/frmAtivacaoConta.cs
@@ -16,22 +16,36 @@ namespace TCCControleDeAcesso.Views
 
     public partial class frmAtivacaoConta : Form
     {
+        CadastroEmpresas _cadastroEmpresas;
 
 
-        public frmAtivacaoConta()
+
+        public frmAtivacaoConta(string text1, string text2, string text3)
         {
             InitializeComponent();
+            this.nome = text1;
+            this.email = text2;
+            this.senha = text3;
         }
 
         public string propriedade {  get; set; }
+        public string senha { get; set; }
+        public string email{ get; set; }
+        public string nome { get; set; }
+
+            
 
         private void frmAtivacaoConta_Load(object sender, EventArgs e)
         {
+
+
             if (!this.propriedade.Equals(""))
             {
 
                 textBox1.Text = this.propriedade; // trazendo o valor do randonCode para a variavel codigoAtivacao
                 string randonCode = textBox1.Text;
+
+
 
 
             }
@@ -43,22 +57,34 @@ namespace TCCControleDeAcesso.Views
             //    int VarAtivacao = txtAtivacao.Text;
             if (txtAtivacao.Text == (textBox1.Text).ToString())
             {
-                MessageBox.Show("Leonidas baitchola", "Verdadeiro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Sua conta foi ativada!", "Ativação da conta", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                frmLogin rp = new frmLogin();
+                this.Hide();
+                rp.Show();
+                
 
+
+                string txtEmail = this.email;
+                string txtNome = this.nome;
+                string txtSenha = this.senha;
+
+                _cadastroEmpresas = new CadastroEmpresas()
+                {
+
+                    Email = txtEmail,
+                    Name = txtNome,
+                    Senha = txtSenha,
+
+                };
+                _cadastroEmpresas.Insert();
             }
             else
             {
                 MessageBox.Show("errou vacilão", "jkhdsakjdhsaj    ", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-            /*  frmAtivacaoConra (int randonCode){
+            
 
-              }
-
-              if ()
-              {
-                  _cadastroEmpresas.Insert();
-              }
-            */
+        
         }
     }
     
