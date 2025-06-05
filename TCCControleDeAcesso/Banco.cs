ab0c72de8e555dcea7baa1b290b299d88950cc7e
Rafaz-dev <114362947+Rxfinha-dev@users.noreply.github.com>
2025-06-05 19:23:11 -0300

    Arrumando Tudo

---
+++ TCCControleDeAcesso/Banco.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace TCCControleDeAcesso.Models
+{
+    public class Banco
+    {
+
+        public static MySqlConnection Connection { get; set; }
+        public static MySqlCommand Command { get; set; }
+        public static MySqlDataAdapter DataAdapter { get; set; }
+        public static DataTable datTable { get; set; }
+
+
+        public static void OpenConnection()
+        {
+            try
+            {
+                Connection = new MySqlConnection("server=localhost;port=3306;uid=root;pwd=etecjau;database=AccessControl;");
+
+
+
+
+                Connection.Open();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Erro Ao Conectar com o Banco", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        public static void CloseConnection()
+        {
+            try
+            {
+                Connection.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Erro Ao Fechar a Conexão com o Banco", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public static void CreateDatabase()
+        {
+            try
+            {
+                //abre a conexão com o banco de dados
+                OpenConnection();
+
+
+
+                Command = new MySqlCommand("CREATE DATABASE IF NOT EXISTS AccessControl", Connection);
+
+                Command.ExecuteNonQuery();
+
+                Command = new MySqlCommand("use accesscontrol", Connection);
+
+
+
+                // Criação da tabela 'escolas'
+                Command = new MySqlCommand("create table if not exists escolas" +
+                    "(id integer auto_increment primary key," +
+                    "nome varchar(50)," +
+                    "email varchar(80)," +
+                    "senha char(8))", Connection);
+                Command.ExecuteNonQuery();
+
+                // Criação da tabela 'cursos'
+                Command = new MySqlCommand("create table if not exists cursos" +
+                    "(id integer primary key auto_increment," +
+                    "nome varchar(30)," +
+                    "idEscola int," +
+                    "foreign key (idEscola) references escolas (id))", Connection);
+                Command.ExecuteNonQuery();
+
+
+                // Criação da tabela 'alunos'
+                Command = new MySqlCommand("create table if not exists alunos " +
+                    "(id integer auto_increment primary key," +
+                    "nome varchar(50)," +
+                    "rm varchar(8)," +
+                    "idade char(3)," +
+                    "Curso varchar(30)," +
+                    "idEscola int," +
+                    "serie char(7)," +
+                    "foto longblob, " +
+                    "digital blob," +
+                    "foreign key (idEscola) references escolas (id))", Connection);
+                Command.ExecuteNonQuery();
+
+
+
+
+
+                // Criação da tabela 'entradas'
+                Command = new MySqlCommand("create table if not exists entradas " +
+                    "(id integer primary key auto_increment," +
+                    "idAluno int," +
+                    "dataEntrada timestamp," +
+                    "idEscola int," +
+                    "foreign key(idAluno) references alunos(id)," +
+                    "foreign key(idEscola) references escolas(id))", Connection);
+                Command.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
---
+++ TCCControleDeAcesso/Views/frmListaAlunos.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace TCCControleDeAcesso.Views
+{
+    partial class frmListaAlunos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtIdOficial = new System.Windows.Forms.TextBox();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.linkSelectPhoto = new System.Windows.Forms.LinkLabel();
+            this.dgvAlunos = new System.Windows.Forms.DataGridView();
+            this.txtIdade = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.BtnDelete = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.cboCurso = new System.Windows.Forms.ComboBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtRm = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAlunos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtIdOficial
+            // 
+            this.txtIdOficial.HideSelection = false;
+            this.txtIdOficial.Location = new System.Drawing.Point(283, 183);
+            this.txtIdOficial.Name = "txtIdOficial";
+            this.txtIdOficial.Size = new System.Drawing.Size(100, 20);
+            this.txtIdOficial.TabIndex = 80;
+            this.txtIdOficial.Visible = false;
+            // 
+            // txtId
+            // 
+            this.txtId.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtId.Location = new System.Drawing.Point(-292, -152);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(283, 13);
+            this.txtId.TabIndex = 79;
+            // 
+            // linkSelectPhoto
+            // 
+            this.linkSelectPhoto.AutoSize = true;
+            this.linkSelectPhoto.LinkColor = System.Drawing.Color.Black;
+            this.linkSelectPhoto.Location = new System.Drawing.Point(857, 396);
+            this.linkSelectPhoto.Name = "linkSelectPhoto";
+            this.linkSelectPhoto.Size = new System.Drawing.Size(72, 13);
+            this.linkSelectPhoto.TabIndex = 78;
+            this.linkSelectPhoto.TabStop = true;
+            this.linkSelectPhoto.Text = "Escolher Foto";
+            this.linkSelectPhoto.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkSelectPhoto_LinkClicked);
+            // 
+            // dgvAlunos
+            // 
+            this.dgvAlunos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAlunos.Location = new System.Drawing.Point(167, 430);
+            this.dgvAlunos.Name = "dgvAlunos";
+            this.dgvAlunos.Size = new System.Drawing.Size(925, 344);
+            this.dgvAlunos.TabIndex = 77;
+            this.dgvAlunos.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvAlunos_CellClick);
+            // 
+            // txtIdade
+            // 
+            this.txtIdade.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIdade.Location = new System.Drawing.Point(283, 268);
+            this.txtIdade.Name = "txtIdade";
+            this.txtIdade.Size = new System.Drawing.Size(493, 24);
+            this.txtIdade.TabIndex = 76;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(231, 272);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 18);
+            this.label6.TabIndex = 75;
+            this.label6.Text = "Idade:";
+            // 
+            // BtnDelete
+            // 
+            this.BtnDelete.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.BtnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnDelete.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.BtnDelete.Location = new System.Drawing.Point(556, 342);
+            this.BtnDelete.Margin = new System.Windows.Forms.Padding(0);
+            this.BtnDelete.Name = "BtnDelete";
+            this.BtnDelete.Size = new System.Drawing.Size(99, 55);
+            this.BtnDelete.TabIndex = 74;
+            this.BtnDelete.Text = "EXCLUIR";
+            this.BtnDelete.UseVisualStyleBackColor = false;
+            this.BtnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "1° Ano",
+            "2° Ano",
+            "3° Ano"});
+            this.comboBox2.Location = new System.Drawing.Point(573, 296);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(203, 26);
+            this.comboBox2.TabIndex = 73;
+            // 
+            // cboCurso
+            // 
+            this.cboCurso.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboCurso.FormattingEnabled = true;
+            this.cboCurso.Location = new System.Drawing.Point(283, 296);
+            this.cboCurso.Name = "cboCurso";
+            this.cboCurso.Size = new System.Drawing.Size(203, 26);
+            this.cboCurso.TabIndex = 72;
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.btnSave.Location = new System.Drawing.Point(677, 343);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(0);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(99, 54);
+            this.btnSave.TabIndex = 70;
+            this.btnSave.Text = "SALVAR";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // txtRm
+            // 
+            this.txtRm.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRm.Location = new System.Drawing.Point(283, 239);
+            this.txtRm.Name = "txtRm";
+            this.txtRm.Size = new System.Drawing.Size(493, 24);
+            this.txtRm.TabIndex = 69;
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(283, 211);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(493, 24);
+            this.txtName.TabIndex = 68;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(521, 301);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 18);
+            this.label4.TabIndex = 67;
+            this.label4.Text = "Série:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(231, 243);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 18);
+            this.label3.TabIndex = 66;
+            this.label3.Text = "RM:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(230, 300);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 18);
+            this.label2.TabIndex = 65;
+            this.label2.Text = "Curso:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(231, 214);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 18);
+            this.label1.TabIndex = 64;
+            this.label1.Text = "Nome:";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::TCCControleDeAcesso.Properties.Resources.Texto_do_seu_parágrafo1;
+            this.pictureBox2.Location = new System.Drawing.Point(397, -261);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(610, 433);
+            this.pictureBox2.TabIndex = 81;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(812, 211);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(185, 191);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 71;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // frmListaAlunos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1523, 861);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.txtIdOficial);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.linkSelectPhoto);
+            this.Controls.Add(this.dgvAlunos);
+            this.Controls.Add(this.txtIdade);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.BtnDelete);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.cboCurso);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtRm);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmListaAlunos";
+            this.Text = "frmListaAlunos";
+            this.Load += new System.EventHandler(this.frmListaAlunos_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAlunos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox txtIdOficial;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.LinkLabel linkSelectPhoto;
+        private System.Windows.Forms.DataGridView dgvAlunos;
+        private System.Windows.Forms.TextBox txtIdade;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button BtnDelete;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox cboCurso;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtRm;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
