fd18501567518027aa687e4d9251b7bd23ae6336
231284 <231284@CPD03-M07>
2025-10-13 15:50:11 -0300

    mexendo no MenuPrincipal

--- TCCControleDeAcesso/Views/MenuPrincipal.Designer.cs
+++ TCCControleDeAcesso/Views/MenuPrincipal.Designer.cs
@@ -31,151 +31,214 @@
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.btnVerificao = new System.Windows.Forms.Panel();
-            this.panel4 = new System.Windows.Forms.Panel();
-            this.panel5 = new System.Windows.Forms.Panel();
-            this.panel6 = new System.Windows.Forms.Panel();
+            this.btnVoltar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnLog = new System.Windows.Forms.Button();
+            this.btnConfig = new System.Windows.Forms.Button();
+            this.btnAluno = new System.Windows.Forms.Button();
+            this.btnCurso = new System.Windows.Forms.Button();
+            this.btnVerificao = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
             this.flowLayoutPanel1.SuspendLayout();
             this.panel2.SuspendLayout();
-            this.btnVerificao.SuspendLayout();
-            this.panel4.SuspendLayout();
-            this.panel5.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // flowLayoutPanel1
             // 
             this.flowLayoutPanel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.flowLayoutPanel1.Controls.Add(this.panel1);
+            this.flowLayoutPanel1.Controls.Add(this.panel2);
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(226, 794);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(218, 1038);
             this.flowLayoutPanel1.TabIndex = 0;
             // 
             // panel1
             // 
             this.panel1.Location = new System.Drawing.Point(3, 3);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(223, 128);
+            this.panel1.Size = new System.Drawing.Size(215, 655);
             this.panel1.TabIndex = 0;
             // 
             // panel2
             // 
-            this.panel2.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.panel2.Controls.Add(this.button3);
-            this.panel2.Location = new System.Drawing.Point(434, 243);
+            this.panel2.Controls.Add(this.btnVoltar);
+            this.panel2.Controls.Add(this.btnSair);
+            this.panel2.Location = new System.Drawing.Point(3, 664);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(223, 61);
+            this.panel2.Size = new System.Drawing.Size(223, 374);
             this.panel2.TabIndex = 1;
+            // 
+            // btnVoltar
+            // 
+            this.btnVoltar.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnVoltar.FlatAppearance.BorderSize = 0;
+            this.btnVoltar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVoltar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVoltar.ForeColor = System.Drawing.Color.Black;
+            this.btnVoltar.Location = new System.Drawing.Point(2, 180);
+            this.btnVoltar.Name = "btnVoltar";
+            this.btnVoltar.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btnVoltar.Size = new System.Drawing.Size(208, 84);
+            this.btnVoltar.TabIndex = 46;
+            this.btnVoltar.Text = "Voltar";
+            this.btnVoltar.UseVisualStyleBackColor = false;
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnSair.FlatAppearance.BorderSize = 0;
+            this.btnSair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSair.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.ForeColor = System.Drawing.Color.Black;
+            this.btnSair.Location = new System.Drawing.Point(-3, 269);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btnSair.Size = new System.Drawing.Size(215, 76);
+            this.btnSair.TabIndex = 45;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = false;
+            // 
+            // btnLog
+            // 
+            this.btnLog.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnLog.FlatAppearance.BorderSize = 0;
+            this.btnLog.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLog.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLog.ForeColor = System.Drawing.Color.Black;
+            this.btnLog.Image = global::TCCControleDeAcesso.Properties.Resources.setting;
+            this.btnLog.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLog.Location = new System.Drawing.Point(5, 448);
+            this.btnLog.Name = "btnLog";
+            this.btnLog.Size = new System.Drawing.Size(210, 67);
+            this.btnLog.TabIndex = 44;
+            this.btnLog.Text = "Configurações";
+            this.btnLog.UseVisualStyleBackColor = false;
+            // 
+            // btnConfig
+            // 
+            this.btnConfig.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnConfig.FlatAppearance.BorderSize = 0;
+            this.btnConfig.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConfig.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConfig.ForeColor = System.Drawing.Color.Black;
+            this.btnConfig.Image = global::TCCControleDeAcesso.Properties.Resources.search__2_;
+            this.btnConfig.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnConfig.Location = new System.Drawing.Point(5, 377);
+            this.btnConfig.Name = "btnConfig";
+            this.btnConfig.Size = new System.Drawing.Size(210, 65);
+            this.btnConfig.TabIndex = 43;
+            this.btnConfig.Text = "Entradas";
+            this.btnConfig.UseVisualStyleBackColor = false;
+            // 
+            // btnAluno
+            // 
+            this.btnAluno.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnAluno.FlatAppearance.BorderSize = 0;
+            this.btnAluno.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAluno.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAluno.ForeColor = System.Drawing.Color.Black;
+            this.btnAluno.Image = global::TCCControleDeAcesso.Properties.Resources.student;
+            this.btnAluno.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAluno.Location = new System.Drawing.Point(5, 295);
+            this.btnAluno.Name = "btnAluno";
+            this.btnAluno.Size = new System.Drawing.Size(210, 76);
+            this.btnAluno.TabIndex = 42;
+            this.btnAluno.Text = "Alunos";
+            this.btnAluno.UseVisualStyleBackColor = false;
+            // 
+            // btnCurso
+            // 
+            this.btnCurso.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnCurso.FlatAppearance.BorderSize = 0;
+            this.btnCurso.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCurso.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCurso.ForeColor = System.Drawing.Color.Black;
+            this.btnCurso.Image = global::TCCControleDeAcesso.Properties.Resources.open_book;
+            this.btnCurso.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCurso.Location = new System.Drawing.Point(5, 216);
+            this.btnCurso.Name = "btnCurso";
+            this.btnCurso.Size = new System.Drawing.Size(210, 73);
+            this.btnCurso.TabIndex = 41;
+            this.btnCurso.Text = "Cursos";
+            this.btnCurso.UseVisualStyleBackColor = false;
             // 
             // btnVerificao
             // 
             this.btnVerificao.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btnVerificao.Controls.Add(this.button2);
-            this.btnVerificao.Cursor = System.Windows.Forms.Cursors.NoMoveVert;
-            this.btnVerificao.Location = new System.Drawing.Point(434, 176);
+            this.btnVerificao.FlatAppearance.BorderSize = 0;
+            this.btnVerificao.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnVerificao.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVerificao.ForeColor = System.Drawing.Color.Black;
+            this.btnVerificao.Image = global::TCCControleDeAcesso.Properties.Resources.id;
+            this.btnVerificao.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnVerificao.Location = new System.Drawing.Point(5, 137);
             this.btnVerificao.Name = "btnVerificao";
-            this.btnVerificao.Size = new System.Drawing.Size(223, 61);
-            this.btnVerificao.TabIndex = 2;
-            this.btnVerificao.Tag = "fdfd";
+            this.btnVerificao.Size = new System.Drawing.Size(210, 73);
+            this.btnVerificao.TabIndex = 40;
+            this.btnVerificao.Text = " Verificação";
+            this.btnVerificao.UseVisualStyleBackColor = false;
             // 
-            // panel4
+            // panel3
             // 
-            this.panel4.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.panel4.Controls.Add(this.button4);
-            this.panel4.Location = new System.Drawing.Point(434, 310);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(223, 61);
-            this.panel4.TabIndex = 2;
-            // 
-            // panel5
-            // 
-            this.panel5.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.panel5.Controls.Add(this.button5);
-            this.panel5.Location = new System.Drawing.Point(434, 377);
-            this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(223, 61);
-            this.panel5.TabIndex = 2;
-            // 
-            // panel6
-            // 
-            this.panel6.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.panel6.Location = new System.Drawing.Point(434, 444);
-            this.panel6.Name = "panel6";
-            this.panel6.Size = new System.Drawing.Size(223, 61);
-            this.panel6.TabIndex = 2;
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Controls.Add(this.button2);
+            this.panel3.Location = new System.Drawing.Point(428, 202);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(223, 374);
+            this.panel3.TabIndex = 47;
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(431, 89);
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(2, 212);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(223, 61);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Verificação";
+            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.button1.Size = new System.Drawing.Size(208, 70);
+            this.button1.TabIndex = 46;
+            this.button1.Text = "Voltar";
             this.button1.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(77, 25);
+            this.button2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(-3, 287);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(107, 15);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 5;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(129, 16);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 6;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(77, 17);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
-            this.button5.TabIndex = 7;
-            this.button5.Text = "button5";
-            this.button5.UseVisualStyleBackColor = true;
+            this.button2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.button2.Size = new System.Drawing.Size(215, 76);
+            this.button2.TabIndex = 45;
+            this.button2.Text = "Sair";
+            this.button2.UseVisualStyleBackColor = false;
             // 
             // MenuPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1080, 794);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.panel4);
-            this.Controls.Add(this.panel5);
-            this.Controls.Add(this.panel6);
+            this.ClientSize = new System.Drawing.Size(1349, 1038);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.btnLog);
+            this.Controls.Add(this.btnConfig);
+            this.Controls.Add(this.btnAluno);
+            this.Controls.Add(this.btnCurso);
             this.Controls.Add(this.btnVerificao);
-            this.Controls.Add(this.panel2);
             this.Controls.Add(this.flowLayoutPanel1);
             this.Name = "MenuPrincipal";
             this.Text = "MenuPrincipal";
             this.flowLayoutPanel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
-            this.btnVerificao.ResumeLayout(false);
-            this.panel4.ResumeLayout(false);
-            this.panel5.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -184,15 +247,16 @@
 
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnLog;
+        private System.Windows.Forms.Button btnConfig;
+        private System.Windows.Forms.Button btnAluno;
+        private System.Windows.Forms.Button btnCurso;
+        private System.Windows.Forms.Button btnVerificao;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Panel btnVerificao;
-        private System.Windows.Forms.Panel panel4;
-        private System.Windows.Forms.Panel panel5;
-        private System.Windows.Forms.Panel panel6;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button btnVoltar;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
